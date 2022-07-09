5363e0d57ee78ccebe2e8fbb4db4f90ebe4581c1
AramAnanyan <aramo.ananyan@gmail.com>
2022-07-09 14:21:34 +0400

    Add project files.

---
+++ sort_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace sort_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int sum = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[sum];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            for(int i=0; i<arr.Length-1;i++)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    max = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = max;
+                    i--;
+                    continue;
+                }
+            }
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ stage1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace stage1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0, k = 0, x=0;
+            int sum=Convert.ToInt32(Console.ReadLine());
+            int[] arr= new int[sum];
+            for(int i=0; i<arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for(int i=0; i<arr.Length; i++)
+            {
+                for(int j=i; j<arr.Length; j++)
+                {
+                    if (arr[j] > max)
+                    {
+                        max = arr[j];
+                        k = j;
+                    }
+                }
+                x = arr[i];
+                arr[i] = max;
+                arr[k] = x;
+                max = 0;
+                k = 0;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
