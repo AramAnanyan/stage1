5788968a08d645d416857905591a793753935019
AramAnanyan <aramo.ananyan@gmail.com>
2022-07-11 16:30:33 +0400

    parse

---
+++ parse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace parse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int x;
+            if (int.TryParse(a, out x))
+            {
+                Console.WriteLine(int.Parse(a));
+            }
+            else Console.WriteLine($"Attempted conversion of '{a}' failed.");
+        }
+    }
+}
--- sort_2/Program.cs
+++ sort_2/Program.cs
@@ -15,7 +15,7 @@ namespace sort_2
             }
 
 
-            for(int i=0; i<arr.Length-1;i++)
+            for(int i=1; i<arr.Length-1;i++)
             {
                 if (arr[i] < arr[i + 1])
                 {
